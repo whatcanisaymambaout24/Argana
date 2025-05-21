0b334291bb4b34a9c7da149a244b7847ace0fe3b
whatcanisaymambaout24 <arganaph17@gmail.com>
2025-05-21 14:08:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("👽haloo")
 st.write(
-    "kamu kerenn[docs.streamlit.io](https://docs.streamlit.io/)."
+    "kamu kerenn [docs.streamlit.io](https://docs.streamlit.io/)."
 )
