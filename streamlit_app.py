43b21c0916bd8c1c15f06541ece3035ed6451e4e
whatcanisaymambaout24 <arganaph17@gmail.com>
2025-05-21 14:08:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,5 @@ import streamlit as st
 
 st.title("👽haloo")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "kamu kerenn[docs.streamlit.io](https://docs.streamlit.io/)."
 )
