7921ff4fb2eb271a7c6baefa233cdbe1c80fdbcb
whatcanisaymambaout24 <arganaph17@gmail.com>
2025-05-21 14:27:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,7 @@ import streamlit as st
 st.title("👽haloo")
 st.write(
     "ini adalah orang terkerenn dibumi
-
-    [docs.streamlit.io](https://docs.streamlit.io/)."
+ [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
 st.image("IMG-20250519-WA0001.jpg", width=200) 
