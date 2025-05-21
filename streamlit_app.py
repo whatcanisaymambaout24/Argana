82ad0ffd5f5bee6a3dcaf5e91fb4f90965bb7b86
whatcanisaymambaout24 <arganaph17@gmail.com>
2025-05-21 14:15:02 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import streamlit as st
 st.title("👽haloo")
 st.write(
     "kamu kerenn [docs.streamlit.io](https://docs.streamlit.io/)."
-)https://github.com/whatcanisaymambaout24/Argana/blob/main/IMG-20250519-WA0001.jpg
+)
+
+St.image("https://github.com/whatcanisaymambaout24/Argana/blob/main/IMG-20250519-WA0001.jpg", width=200) 
